074a0fdaaa76de31b2fe0d3357dc167dceadb54e
tmaeda11235 <tmaeda11235@gmail.com>
2017-08-05 16:43:17 +0900

    modified:   __pycache__/__init__.cpython-35.pyc 	modified:   __pycache__/laplasian.cpython-35.pyc 	new file:   __pycache__/mesh.cpython-35.pyc 	new file:   __pycache__/nelson.cpython-35.pyc 	modified:   __pycache__/potential.cpython-35.pyc 	modified:   __pycache__/schroedinger.cpython-35.pyc 	modified:   __pycache__/state.cpython-35.pyc 	modified:   mesh.py 	new file:   note.py

--- schrpy/mesh.py
+++ schrpy/mesh.py
@@ -1,7 +1,7 @@
 from scipy import arange, meshgrid
 
 
-class mesh:
+class Mesh:
 
     def __init__(self, xmin, xmax, dx, t0, tmax, dt):
         self.xmin = xmin
@@ -10,9 +10,10 @@ class mesh:
         self.t0 = t0
         self.tmax = tmax
         self.dt = dt
+        self.param = (xmin, xmax, dx, t0, tmax, dt)
 
-        self.x_vector = arange(self.xmin, self.xmax, self.dx)
-        self.t_vector = arange(self.t0, self.tmax, self.dt)
+        self.x_vector = arange(xmin, xmax, dx)
+        self.t_vector = arange(t0, tmax, dt)
         self.x_num = len(self.x_vector)
         self.t_num = len(self.t_vector)
         self.x_matrix, self.t_matrix = meshgrid(self.x_vector, self.t_vector)
@@ -23,8 +24,11 @@ class mesh:
         t_string = "{}<t<{}  (dt={})".format(self.t0, self.tmax, self.dx)
         return "{}\n{}".format(x_string, t_string)
 
+    def __eq__(self, other):
+        return self.param == other.param
 
-class MyMesh(mesh):
+
+class MyMesh(Mesh):
 
     def __init__(self, xmin=-60, xmax=60, dx=0.02, t0=0, tmax=20, dt=0.02):
         super(MyMesh, self).__init__(xmin, xmax, dx, t0, tmax, dt)
---
+++ schrpy/note.py
@@ -0,0 +1,6 @@
+import schrpy as sp
+
+class note:
+
+    def __init__(self, xmin, xmax, dx, t0, tmax, dt)
+    sp.mesh.mesh(xmin, xmax, dx, t0, tmax, dt)
