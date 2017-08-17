4bb60a00935b74a102c7be294da7f945f1345d19
tmaeda11235 <tmaeda11235@gmail.com>
2017-08-17 23:05:31 +0900

    Merge branch 'irohas' of https://github.com/tmaeda11235/schroedinger_solver into irohas
    # Conflicts:
    #	schrpy/__init__.py
    #	schrpy/mesh.py

--- notetest3.py
+++ notetest3.py
@@ -1,10 +1,19 @@
 from schrpy import *
+from scipy.integrate import trapz
+import matplotlib.pyplot as plt
+from scipy import *
 from time import time
 from scipy import save
-mesh = Mesh(-10, 10, 0.05, 0, 10, 0.05)
+mesh = Mesh(-15, 10, 0.05, 0, 120, 0.01)
 note = Note(mesh)
-note.set_potential(Potential(mesh, lambda y: 5 * (0 < y) * (y < 1) + 5 * (3 < y) * (y < 4)))
-note.set_initial(GaussianState(mesh, -3, 3, 3))
+note.set_potential(Potential(mesh, lambda y: 6 * ((0 < y) * (y < 1) + (3 < y) * (y < 4))))
+note.set_initial(GaussianState(mesh, -5, 3, 3))
 note.solve_schroedinger_equation(boundary="absorb")
-note.easy_plot2d(save=True, title="aaaaaaa")
-
+note.easy_plot2d(save=True, title="Double-well")
+s = trapz(absolute(note.solution[:, 310:370])**2)
+plt.plot(mesh.t_vector, s)
+plt.xlabel("time")
+plt.ylabel("P(t)")
+plt.yscale("log")
+plt.xscale("log")
+plt.show()
