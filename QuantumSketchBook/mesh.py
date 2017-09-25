dcde32fc1dd55ed023a274b6b61dac8f65384cb2
tmaeda11235 <tmaeda11235@gmail.com>
2017-09-25 16:05:05 +0900

    remake mesh using collection.namedtupple

--- QuantumSketchBook/mesh.py
+++ QuantumSketchBook/mesh.py
@@ -1,30 +1,44 @@
 from scipy import arange
 from numbers import Real
+from collections import namedtuple
 
 
-class Mesh:
+class Mesh(namedtuple("_Mesh", "x_min, x_max, dx, t_min, t_max, dt")):
 
-    def __init__(self, x_min, x_max, dx, t_min, t_max, dt):
+    def __new__(cls, x_min, x_max, dx, t_min, t_max, dt):
         if not all(isinstance(x, Real) for x in (x_min, x_max, dx, t_min, t_max, dt)):
-            raise TypeError
-        if not (x_min < x_max or t_min < t_max):
-            raise ValueError("The min should be shorter than The max. ")
+            raise TypeError("input should be a number")
+        if not (x_min < x_max and t_min < t_max):
+            raise ValueError("The min should be smaller than The max. ")
         if not (0 < dx or 0 < dt):
             raise ValueError("The dx and dt should be positive. ")
+        if not (x_max - x_min > dx and t_max - t_min > dt):
+            raise ValueError("too big to make mesh. ")
+        return super().__new__(cls, x_min, x_max, dx, t_min, t_max, dt)
 
-        self.x_min = x_min
-        self.x_max = x_max
-        self.dx = dx
-        self.t_min = t_min
-        self.t_max = t_max
-        self.dt = dt
-        self.param = (x_min, x_max, dx, t_min, t_max, dt)
+    @property
+    def param(self):
+        return self.x_min, self.x_max, self.dx, self.t_min, self.t_max, self.dt
 
-        self.x_vector = arange(x_min, x_max, dx)
-        self.t_vector = arange(t_min, t_max, dt)
-        self.x_num = len(self.x_vector)
-        self.t_num = len(self.t_vector)
-        self.dense = (self.dx ** -1, self.dt ** -1)
+    @property
+    def x_vector(self):
+        return arange(self.x_min, self.x_max, self.dx)
+
+    @property
+    def t_vector(self):
+        return arange(self.t_min, self.t_max, self.dt)
+
+    @property
+    def x_num(self):
+        return len(self.x_vector)
+
+    @property
+    def t_num(self):
+        return len(self.t_vector)
+
+    @property
+    def dense(self):
+        return self.dx ** -1, self.dt ** -1
 
     def __str__(self):
         x_string = "{}<x<{}  (dx={})".format(self.x_min, self.x_max, self.dt)
@@ -35,5 +49,8 @@ class Mesh:
         return self.param == other.param
 
 
-def my_mesh(self, x_min=-60, x_max=60, dx=0.02, t_min=0, t_max=20, dt=0.02):
+def my_mesh(x_min=-10, x_max=10, dx=0.1, t_min=0, t_max=10, dt=0.1):
     return Mesh(x_min, x_max, dx, t_min, t_max, dt)
+
+if __name__ == "__main__":
+    print(my_mesh())
