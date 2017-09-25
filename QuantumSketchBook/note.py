f04d55f83623ad125b7f40a86be8cee346df3220
tmaeda11235 <tmaeda11235@gmail.com>
2017-09-25 19:12:33 +0900

    refactor import

--- QuantumSketchBook/note.py
+++ QuantumSketchBook/note.py
@@ -1,59 +1,62 @@
-import QuantumSketchBook as sp
-from QuantumSketchBook.potential import Potential
-from QuantumSketchBook.state import State
-import scipy as sc
+from QuantumSketchBook.mesh import Mesh
+from QuantumSketchBook.nelson import Nelson
+from QuantumSketchBook.schroedinger import Schroedinger
+from QuantumSketchBook.hamiltonian import Hamiltonian
+from QuantumSketchBook.potential import Potential, potential
+from QuantumSketchBook.state import State, gaussian_state
+from scipy import load, save, meshgrid, absolute
 from matplotlib.pyplot import figure
 from matplotlib.colors import Normalize
 
 
 class Note:
 
-    def __init__(self, mesh, mass=1):
+    def __init__(self, mesh: Mesh, mass=1):
         self.mesh = mesh
-        self.potential = sp.potential(self.mesh, lambda x: 0 * x)
-        self.initial = sp.gaussian_state(self.mesh, 0, 1, 0)
-        self.hamiltonian = sp.Hamiltonian(self.mass, self.potential, mass=mass)
-        self.schroedinger = sp.Schroedinger(self.hamiltonian, self.initial)
+        self.potential = potential(self.mesh, lambda x: 0 * x)
+        self.initial = gaussian_state(self.mesh, 0, 1, 0)
+        self.hamiltonian = Hamiltonian(self.mass, self.potential, mass=mass)
+        self.schroedinger = Schroedinger(self.hamiltonian, self.initial)
         self.solution = None
         self.locus = None
         self.mass = mass
 
-    def set_potential(self, potential: Potential):
-        if not potential.mesh.param == self.mesh.param:
+    def set_potential(self, new_potential: Potential):
+        if not new_potential.mesh.param == self.mesh.param:
             raise ValueError("objects based on different meshes have been imputed. ")
-        self.potential = potential
+        self.potential = new_potential
 
-    def set_initial(self, state: State):
-        if not state.mesh.param == self.mesh.param:
+    def set_initial(self, new_state: State):
+        if not new_state.mesh.param == self.mesh.param:
             raise ValueError("objects based on different meshes have been imputed. ")
-        self.initial = state
+        self.initial = new_state
 
     def solve_schroedinger_equation(self, boundary="free"):
-        self.hamiltonian = sp.Hamiltonian(self.mesh, self.potential, mass=self.mass, boundary=boundary)
-        self.schroedinger = sp.Schroedinger(self.hamiltonian, self.initial)
+        self.hamiltonian = Hamiltonian(self.mesh, self.potential, mass=self.mass, boundary=boundary)
+        self.schroedinger = Schroedinger(self.hamiltonian, self.initial)
         self.solution = self.schroedinger.solution()
         return self.solution
 
     def save_solution(self, file_name):
-        sc.save(file_name, self.solution)
+        save(file_name, self.solution)
 
     def load_solution(self, file_name):
-        sol = sc.load(file_name)
+        sol = load(file_name)
         if not sol.shape == (self.mesh.t_num, self.mesh.x_num):
             raise ValueError("Different meshes have been imputed")
         self.solution = sol
 
     def generate_locus(self, n, micro_steps=10):
-        nel = sp.Nelson(self.schroedinger, n, micro_steps=micro_steps)
+        nel = Nelson(self.schroedinger, n, micro_steps=micro_steps)
         self.locus = nel.locus()
         return self.locus
 
     def easy_plot2d(self, max_pix=(1000, 1000), show=True, save=False, title="no_title", limit=()):
         x_step = int(self.mesh.x_num / max_pix[0]) + 1
         t_step = int(self.mesh.x_num / max_pix[1]) + 1
-        x_grid, t_grid = sc.meshgrid(self.mesh.x_vector[::x_step], self.mesh.t_vector[::t_step])
-        phi2 = sc.absolute(self.solution[1::t_step, ::x_step]) ** 2
-        potential = self.potential.vector
+        x_grid, t_grid = meshgrid(self.mesh.x_vector[::x_step], self.mesh.t_vector[::t_step])
+        phi2 = absolute(self.solution[1::t_step, ::x_step]) ** 2
+        potential_ = self.potential.vector
         x = self.mesh.x_vector
         norm = Normalize(vmax=phi2[0].max())
         fig = figure()
@@ -67,7 +70,7 @@ class Note:
         under.set_xlabel("space(a.u.)")
 
         above.pcolormesh(x_grid, t_grid, phi2, cmap="nipy_spectral_r", norm=norm)
-        under.plot(x, potential, ".")
+        under.plot(x, potential_, ".")
 
         if limit is not ():
             above.set_xlim(*limit)
