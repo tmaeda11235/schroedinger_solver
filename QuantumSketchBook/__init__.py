b12415610806bebf44fa088a896c105c3fad961f
tmaeda11235 <tmaeda11235@gmail.com>
2017-09-26 20:10:56 +0900

    fix __init__.py

--- QuantumSketchBook/__init__.py
+++ QuantumSketchBook/__init__.py
@@ -1,9 +1,6 @@
 from QuantumSketchBook.mesh import Mesh, my_mesh  # noqa
 from QuantumSketchBook.context import MeshContext # noqa
-from QuantumSketchBook.laplasian import Laplasian  # noqa
-from QuantumSketchBook.state import State, gaussian_state# noqa
-from QuantumSketchBook.potential import potential, free,  box, kp, step, vacuum_kp, kp_vacuum  # noqa
-from QuantumSketchBook.schroedinger import Schroedinger  # noqa
-from QuantumSketchBook.nelson import Nelson  # noqa
+from QuantumSketchBook.state import State, state, gaussian_state# noqa
+from QuantumSketchBook.potential import Potential, potential, free,  box, kp, step, vacuum_kp, kp_vacuum  # noqa
 from QuantumSketchBook.note import Note  # noqa
 from QuantumSketchBook.hamiltonian import Hamiltonian  # noqa
